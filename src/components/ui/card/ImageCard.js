532d5a9c36ff544a498b6076d3eb1ff4590af26a
eugen-io <94427178+eugen-io@users.noreply.github.com>
2023-03-16 13:32:25 +0800

    Fixed aspect ratio

--- src/components/ui/card/ImageCard.js
+++ src/components/ui/card/ImageCard.js
@@ -5,7 +5,7 @@ import ImageModal from "../modal/ImageModal";
 export function ImageCard({img, imgDescription, title, subtitle, className, children}) {
     return (
         <div className={classNames(className, "w-full h-full flex flexbox flex-col bg-gray-200 rounded-md p-5")}>
-            <ImageModal src={img} description={imgDescription} className="rounded-md"/>
+            <ImageModal src={img} description={imgDescription} className="rounded-md aspect-[1.414]"/>
             <br />
             <div>
                 <Heading level={2} className="font-bold">{title}</Heading>
